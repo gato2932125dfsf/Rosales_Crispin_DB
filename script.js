9dabccb3069043054a4dda538dc6fb2976c8cac1
gato2932125dfsf <crodriges698@gmail.com>
2026-05-18 18:27:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -865,7 +865,7 @@
     ],
      u2_t07: [
       {
-        name: '   Semana 7 - Desarollo Resumen.png',
+        name: '   Semana 7 - Desarollo Resumen.pdf',
         data: 'carpeta1/Sem7_Resumen.pdf',
         size: null, type: 'pdf', folder: 'carpeta1',
         uploaded: 'Precargado', isStatic: true
